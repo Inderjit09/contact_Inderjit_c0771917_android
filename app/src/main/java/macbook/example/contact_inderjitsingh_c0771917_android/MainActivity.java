679d49df514fb6a123c0db333f22a5f843993d2c
Inderjit Singh <erinderbrar1@gmail.com>
2020-07-15 14:12:03 -0400

    Initial commit

---
+++ app/src/main/java/macbook/example/contact_inderjitsingh_c0771917_android/MainActivity.java
@@ -0,0 +1,14 @@
+package macbook.example.contact_inderjitsingh_c0771917_android;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
